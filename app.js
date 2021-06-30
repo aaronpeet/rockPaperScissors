c1181eadb2ba280245636b136131c87a436dabf8
aaronpeet <aaronpeet@icloud.com>
2021-06-30 17:01:26 -0600

    random function done

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-function rock() {
-    console.log('bam!');
+let weapon = {
+    rock: {
+        beats: 'scissors'
+    },
+    paper: {
+        beats: 'rock'
+    },
+    scissors: {
+        beats: 'paper'
+    }
 }
-rock()
+
+
+
+function play(playerChoice) {
+    let player = playerChoice.weapon
+    let compChoice = choices[randIndex]
+    if (player == compChoice) {
+        document.getElementById('outcome').innerText = "It's a draw.." 
+    }  
+}
+
+
+
+function random() {
+    let choices = Object.keys(weapon)
+    let randIndex = Math.floor(Math.random() * (choices.length))
+    return choices[randIndex]
+}
